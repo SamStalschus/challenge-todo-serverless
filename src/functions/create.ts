0abfeb6ac22007b9f1136e0cdb837777d8be064c
SamStalschus <samuel5stalschus@gmail.com>
2021-06-08 01:18:56 -0300

    feat: add create todos function

--- src/functions/create.ts
+++ src/functions/create.ts
@@ -1,13 +1,51 @@
+import { document } from '../utils/dynamodbClient'
+import { v4 as uuidV4 } from 'uuid'
+
+
+interface ICreateTodo {
+	title?: string
+	deadline?: string
+}
 
 
 export const handle = async (event) => {
 
+  let { title, deadline } = JSON.parse(event.body) as ICreateTodo
+
+  const { user_id } = event.pathParameters
+  
+  if(!user_id || !title || !deadline) {
+    return {
+      statusCode: 404,
+      body: 
+      JSON.stringify({
+        message: "Request missing params!"
+      }),
+    }
+  }
+  
+  const id = uuidV4()
+
+  await document.put({
+    TableName: 'todos',
+    Item: {
+      id,
+      user_id,
+      title,
+      done: false,
+      deadline: new Date(deadline).toISOString()
+    }
+  }).promise()
+
   return {
     statusCode: 201,
-    body: 
-      JSON.stringify({
-        message: "Testeeee"
-      }),
+    body: JSON.stringify({
+      id,
+      user_id,
+      title,
+      done: false,
+      deadline: new Date(deadline),
+    }),
     
     headers: {
       'Content-Type': 'application/json'
